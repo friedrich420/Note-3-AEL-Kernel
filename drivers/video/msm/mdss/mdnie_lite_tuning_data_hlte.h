d077aa9f848dfb7ae2c7075675bfd1a72e3a44b2
yank555-lu <yank555.lu@gmail.com>
2014-04-27 12:29:12 +0200

    mdnie: HIJACK MDNIE Profile v1.0a (Yank555.lu)
    - added WHATHUB's new control interface keeping the current interface at the same time (stay open)
    
    mdnie lite control: revert back to stocklike disabled by default (Yank555.lu)
    
    mdnie lite control: update single control for black level
    
    - black is applied as a delta to actual black RGB values (-128 to +128)
    
    mdnie lite control: add single control for black level
    
    - slightly adapted to keep copy_mode (Yank555.lu)
    - black kept at 0 by default as Natural is kept as base calibration (Yank555.lu)
    
    mdnie lite control: override for all samsung scenarios and profiles (WhatHub)
    
    - slightly adapted to keep copy_mode (Yank555.lu)
    
    mdnie lite control: adjust calibration (WhatHub)
    
    - slightly adapted to keep copy_mode (Yank555.lu)
    
    Simple switchable mdnie lite control interface
    
    - make hijacking STANDARD mode sysfs switchable, disabled by default (Yank555.lu)
    - add copy curve information from existing mode
    - add copy full mode from existing mode
    
    Sysfs Interface : /sys/class/mdnie/mdnie/...
    
    Simple mdnie lite control interface (WhatHub)
    
    - mdnie lite control: use standard mode instead of movie mode
    
    Conflicts:
    	drivers/video/msm/mdss/mdnie_lite_tuning_data_hlte.h

--- drivers/video/msm/mdss/mdnie_lite_tuning_data_hlte.h
+++ drivers/video/msm/mdss/mdnie_lite_tuning_data_hlte.h
@@ -21,6 +21,7 @@
 
 ////////////////// UI /// /////////////////////
 
+<<<<<<< HEAD
 static char SCREEN_CURTAIN_1[] = {
 	//start
 	0xEB,
@@ -73,6 +74,60 @@ static char SCREEN_CURTAIN_2[] = {
 	0x00, //scr Kg Kg
 	0x00, //scr Wb Wr
 	0x00, //scr Kb Kr
+=======
+#if defined(CONFIG_MDNIE_LITE_CONTROL)
+char LITE_CONTROL_1[] = {
+	0xEB,
+	0x01,
+	0x00,
+	0x33,
+	0x00, //4 = sharpen
+};
+
+char LITE_CONTROL_2[] = {
+	0xEC,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00, //18 = cyan_red
+	0xff, //19 = red_red
+	0xff, //20 = cyan_green
+	0x00, //21 = red_green
+	0xff, //22 = cyan_blue
+	0x00, //23 = red_blue
+	0xff, //24 = magenta_red
+	0x00, //25 = green_red
+	0x00, //26 = magenta_green
+	0xff, //27 = green_green
+	0xff, //28 = magenta_blue
+	0x00, //29 = green_blue
+	0xff, //30 = yellow_red
+	0x00, //31 = blue_red
+	0xff, //32 = yellow_green
+	0x00, //33 = blue_green
+	0x00, //34 = yellow_blue
+	0xff, //35 = blue_blue
+	0xff, //36 = white_red
+	0x00, //37 = black_red
+	0xff, //38 = white_green
+	0x00, //39 = black_green
+	0xff, //40 = white_blue
+	0x00, //41 = black_blue
+>>>>>>> c005078... mdnie: HIJACK MDNIE Profile v1.0a (Yank555.lu)
 	0x00, //curve 1 b
 	0x20, //curve 1 a
 	0x00, //curve 2 b
@@ -87,7 +142,11 @@ static char SCREEN_CURTAIN_2[] = {
 	0x1b, //curve 6 a
 	0x02, //curve 7 b
 	0x1b, //curve 7 a
+<<<<<<< HEAD
 	0x02, //curve 8 b
+=======
+	0x01, //curve 8 b
+>>>>>>> c005078... mdnie: HIJACK MDNIE Profile v1.0a (Yank555.lu)
 	0x1b, //curve 8 a
 	0x09, //curve 9 b
 	0xa6, //curve 9 a
@@ -121,6 +180,7 @@ static char SCREEN_CURTAIN_2[] = {
 	0x20, //curve23 a
 	0x00, //curve24 b
 	0xFF, //curve24 a
+<<<<<<< HEAD
 	0x04, //cc r1
 	0x00,
 	0x00, //cc r2
@@ -141,6 +201,28 @@ static char SCREEN_CURTAIN_2[] = {
 	0x00,
 	//end
 };
+=======
+	0x04, //cc r1 0.08x
+	0x39,
+	0x1f, //cc r2
+	0xd0,
+	0x1f, //cc r3
+	0xf7,
+	0x1f, //cc g1
+	0xe8,
+	0x04, //cc g2
+	0x21,
+	0x1f, //cc g3
+	0xf7,
+	0x1f, //cc b1
+	0xe8,
+	0x1f, //cc b2
+	0xd0,
+	0x04, //cc b3
+	0x48,
+};
+#endif
+>>>>>>> c005078... mdnie: HIJACK MDNIE Profile v1.0a (Yank555.lu)
 
 static char STANDARD_UI_1[] = {
 	0xEB,
@@ -738,6 +820,7 @@ char AUTO_UI_2[] = {
 };
 
 ////////////////// GALLERY /////////////////////
+
 static char STANDARD_GALLERY_1[] = {
 	0xEB,
 	0x01, //mdnie_en
